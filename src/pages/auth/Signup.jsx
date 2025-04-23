362f4c993ae6118174f2596b5c8e56900a5d29c5
shakhawat.dev <shakhawat.code@gmail.com>
2025-04-23 17:59:44 +0600

    welcome msg hover change

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -40,7 +40,7 @@ export const Signup = () => {
     <div className={`grid place-items-center h-screen ${color.bg} `}>
         <div className="text-center px-4 ">
                 <h2 className={` flex items-center gap-2 font-ubuntu font-bold text-3xl justify-center ${color.brand}`}><RiMicrosoftLoopFill/><span>FriendLoop</span></h2>
-                <h3 className={` ${ color.hoverbg} ${color.txt} font-medium mt-5 font-opensans`}>Chase your dreams, we're here every step of the way.<br/> Lets create your <span className={`font-ubuntu font-medium ${color.brand} `}>FriendLoop</span> account now. </h3>
+                <h3 className={` ${color.txt} font-medium mt-5 font-opensans`}>Chase your dreams, we're here every step of the way.<br/> Lets create your <span className={`font-ubuntu font-medium ${color.brand} `}>FriendLoop</span> account now. </h3>
 
                 {/* signup form */}
                 <form  className={`p-2 mt-10 `}>
